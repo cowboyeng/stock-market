618a6b759250d555a25550d7a77dbe7916f6a0c8
milankaku <milankaku1@gmail.com>
2018-01-16 14:04:30 -0600

    subplot of 100 day moving average and adjusted close with volume on 2nd axis

--- stock-market.py
+++ stock-market.py
@@ -9,5 +9,13 @@ start = dt.datetime(2014, 1, 1)
 end = dt.datetime(2018, 1, 15)
 
 df = web.DataReader("TSLA", 'yahoo', start, end)
-df[['High', 'Low']].plot()
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
 plt.show()
