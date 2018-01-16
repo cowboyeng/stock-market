c11baabc5d66c76c06ebe423c10308f86889e491
milankaku <milankaku1@gmail.com>
2018-01-16 14:19:52 -0600

    normalize adjusted close to 10 day window. use ohlc to build a candlestick graph of that resampled data with volume

--- stock-market.py
+++ stock-market.py
@@ -3,19 +3,24 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
+from matplotlib.finance import candlestick_ohlc
+import matplotlib.dates as mdates
 
 style.use('ggplot')
 start = dt.datetime(2014, 1, 1)
 end = dt.datetime(2018, 1, 15)
 
 df = web.DataReader("TSLA", 'yahoo', start, end)
-df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+df_ohlc = df['Adj Close'].resample('10D').ohlc()
+df_vol = df['Volume'].resample('10D').sum()
+df_ohlc = df_ohlc.reset_index()
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
 
-
+figure = plt.figure()
 ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
-ax1.plot(df.index, df['Adj Close'])
-ax1.plot(df.index, df['100ma'])
-ax2.bar(df.index, df['Volume'])
+ax1.xaxis_date()
 
+candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
+ax2.fill_between(df_vol.index.map(mdates.date2num), df_vol.values, 0)
 plt.show()
