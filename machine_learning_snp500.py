b45b4ae26930bed47913e6dbb8601d028028919d
milankaku <milankaku1@gmail.com>
2018-01-24 20:32:24 -0600

    get prediction spread and accuracy

--- machine_learning_snp500.py
+++ machine_learning_snp500.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from collections import Counter
+import numpy as np
+from sklearn import svm, cross_validation, neighbors
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 
 
 # Model built on a single company/ticker. Will find the percent change values
@@ -8,12 +12,13 @@ def process_data_for_labels(ticker):
     df = pd.read_csv('sp500_joined_adj_closes.csv', index_col=0)
     tickers = df.columns.values.tolist()
     df.fillna(0, inplace=True)
-
     for i in range(1, how_days + 1):
         df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
     df.fillna(0, inplace=True)
-    print(df.head())
+
+
     return tickers, df
+
 
 # Generate our labels, whether we want to buy sell or hold
 def buy_sell_hold(*args):
@@ -29,4 +34,52 @@ def buy_sell_hold(*args):
     # Hold Price hasn't dropped or raised 2%
     return 0
 
-process_data_for_labels('MMM')
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+
+    df['{}_target'.format(ticker)] = list(map( buy_sell_hold,
+                                               df['{}_1d'.format(ticker)],
+                                               df['{}_2d'.format(ticker)],
+                                               df['{}_3d'.format(ticker)],
+                                               df['{}_4d'.format(ticker)],
+                                               df['{}_5d'.format(ticker)],
+                                               df['{}_6d'.format(ticker)],
+                                               df['{}_7d'.format(ticker)],
+                                               ))
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread:', Counter(str_vals))
+
+    df.fillna(0, inplace=True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df.fillna(0, inplace=True)
+
+    X = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+
+    return X, y, df
+
+
+def do_machine_learning(ticker):
+    X, y, df = extract_featuresets(ticker)
+
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25)
+
+    clf = VotingClassifier([('lsvc', svm.LinearSVC()),
+                            ('knn', neighbors.KNeighborsClassifier()),
+                            ('rfor', RandomForestClassifier())])
+
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print("Accuracy", confidence)
+    predections = clf.predict(X_test)
+    print('Predicted spread:', Counter(predections))
+
+    return confidence
+
+do_machine_learning('MMM')
